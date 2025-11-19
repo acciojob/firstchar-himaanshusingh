d159a634fd97447c15d9be6719d3ad977ca080ac
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-19 12:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstChar(text) {
   // your code here
   if (text.charCodeAt(0) === " " || text.charCodeAt(0) === "") return "";
-  else return String.from(text.charCodeAt(0));
+  else return String.fromCharCode(text.charCodeAt(0));
 }
 
 // Do not change the code below
 // Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
